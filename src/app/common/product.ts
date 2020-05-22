f668e69024a62c863d7bdc9404b1a7a197811991
Marcelo  Silva <mrsalustiano@gmail.com>
2020-05-22 05:27:37 -0300

    Iniciando Projeto Angular

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: String;
+    name: String;
+    description: String;
+    unitPrice: number;
+    imageURL: String;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
