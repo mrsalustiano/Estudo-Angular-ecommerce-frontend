91be6f53453bb1bbc266d7e4b80078af4625617e
Marcelo  Silva <mrsalustiano@gmail.com>
2020-05-27 01:31:07 -0300

    menu dinamico e nome da categoria na pagina

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,17 +11,39 @@ import { Product } from 'src/app/common/product';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
+  currentCategoryName: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
-    this.listProducts();
-
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts(){
-    this.productService.getproductList().subscribe(
+  
+    // check if "id" paramenters is avaliable
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+  
+    if (hasCategoryId){
+      //get the "id"  param string. convert to number using "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
+    } else {
+      // if the "id" not avaliable , set default category to 1
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
+
+    } 
+
+    //now get product from category id
+
+    this.productService.getproductList(this.currentCategoryId).subscribe(
       data => {
         this.products  =data;
       }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,26 +3,46 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
   private baseUrl  = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private HttpClient: HttpClient) { }
 
-  getproductList(): Observable<Product[]>{
-    return this.HttpClient.get<GetResponse>(this.baseUrl).pipe(
+  getproductList(theCategoryId: number): Observable<Product[]>{
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.HttpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.HttpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+
+
+  }
+
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded:  {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded:  {
+    productCategory: ProductCategory[];
   }
 }
